f64f004f90c5874f790a28ede79c83e57bdfd0a7
DanFelix3 <danvfelix313@gmail.com>
2025-07-24 20:24:18 +0530

    Initial commit

---
+++ src/main/java/com/tourmanager/tourismmanager/TourismmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.tourmanager.tourismmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourismmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourmanager/tourismmanager/TourismmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourmanager.tourismmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
